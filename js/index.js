36d57ceaacb1c903e7d7d30db4540bf6d389e685
Nasir Uddin <monforingnasir@gmail.com>
2021-01-29 19:59:08 +0600

    JS Calculator

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
+
 let screenValue = '';
+
 for (let item of buttons) {
+
     item.addEventListener('click', (e) => {
+
         let buttonText = e.target.innerText;
         console.log("click is ", buttonText);
+
         if (buttonText == 'X') {
             buttonText = '*';
             screenValue = screenValue + buttonText;
             screen.value = screenValue;
-        }
-        else if (buttonText == 'C') {
+        } else if (buttonText == 'C') {
             screenValue = "";
             screen.value = screenValue;
-        }
-        else if (buttonText == '=') {
+        } else if (buttonText == '=') {
             screen.value = eval(screenValue);
-        }
-        else {
+        } else {
             screenValue = screenValue + buttonText;
             screen.value = screenValue;
         }
-    })
+    });
 }
